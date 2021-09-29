6f85f79a737e312ca4334e2b3d86c7426f1d8b5f
tuannguyen2706 <85569448+tuannguyen2706@users.noreply.github.com>
2021-09-29 21:54:28 +0700

    assignment 2

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
